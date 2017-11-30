cbb7e3b6fa835bcc5cff605ad6cdf0c31b3580da
Jason Stillwell <dragonfax@gmail.com>
2017-11-30 01:39:24 -0800

    looks up and creates user record

--- lib/pages/signin_page.dart
+++ lib/pages/signin_page.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'dart:async';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:hythr/google_signin.dart';
-// import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_database/firebase_database.dart';
+
 
 class SignInPage extends StatefulWidget {
 
@@ -37,6 +38,13 @@ class SignInState extends State<SignInPage> {
         idToken: credentials.idToken,
         accessToken: credentials.accessToken,
       );
+    }
+
+    // get the user record, or create it.
+    var userDoc = FirebaseDatabase.instance.reference().child('users/' + googleSignIn.currentUser.id);
+    var userSnapshot = await userDoc.once();
+    if (userSnapshot.value == null ) {
+      await userDoc.update({ "real_name": googleSignIn.currentUser.displayName });
     }
 
     // should be logged in by now.
