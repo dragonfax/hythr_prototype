bb6a7a904749ec933e87a3048bac78d3e71a439b
Jason Stillwell <dragonfax@gmail.com>
2018-01-16 15:42:41 -0800

    Add the new note list format, one note per line. Move the text input into the floating action button. Add created at date to notes. Add dialogs for viewing notes individually.

--- lib/pages/client_notes_page.dart
+++ lib/pages/client_notes_page.dart
@@ -1,29 +1,62 @@
 import 'package:flutter/material.dart';
 import 'package:hythr/content/client.dart';
 import 'package:firebase_database/ui/firebase_animated_list.dart';
+import 'package:flutter/widgets.dart';
 import 'page.dart';
 import 'package:hythr/content/user.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'dart:math';
 import 'dart:io';
 import 'package:image_picker/image_picker.dart';
-import 'package:flutter/widgets.dart';
 import 'package:firebase_storage/firebase_storage.dart';
 import 'input_dialog.dart';
+import 'package:flutter_fab_dialer/flutter_fab_dialer.dart';
+
 
 abstract class ClientNote {
-  Widget getWidget();
+  DateTime createdAt;
+
+  Widget getWidget(BuildContext context);
 
   ClientNote();
 
   factory ClientNote.fromFirebaseSnapshot(DataSnapshot snapshot) {
+    ClientNote n;
     if ( snapshot.value['type'] == "text" ) {
-      return new TextNote.fromFirebaseSnapshot(snapshot);
+      n = new TextNote.fromFirebaseSnapshot(snapshot);
     } else if ( snapshot.value['type'] == "photo" ) {
-      return new PhotoNote.fromFirebaseSnapshot(snapshot);
+      n = new PhotoNote.fromFirebaseSnapshot(snapshot);
     } else {
       throw "failed to get note type";
     }
+
+    if (snapshot.value['created_at'] != null) {
+      n.createdAt = new DateTime.fromMillisecondsSinceEpoch(snapshot.value['created_at']);
+    }
+
+    return n;
+  }
+
+  Map toFirebaseSet() {
+    return {
+      "created_at": createdAt == null ? ServerValue.timestamp : createdAt.millisecondsSinceEpoch
+    };
+  }
+
+
+  String formatDate() {
+    if ( createdAt != null ) {
+      return createdAt.month.toString() + '/' + createdAt.day.toString();
+    } else {
+      return '';
+    }
+  }
+
+  Widget getDateWidget() {
+    return new Container(
+      width: 50.0,
+      child: new Text(formatDate())
+    );
   }
 }
 
@@ -36,13 +69,43 @@ class PhotoNote extends ClientNote {
     photoUrl = snapshot.value['photo_url'];
   }
 
+  @override
   toFirebaseSet() {
-    return { "type": "photo", "photo_url": photoUrl };
+    var s = super.toFirebaseSet();
+    s.addAll({ "type": "photo", "photo_url": photoUrl});
+    return s;
   }
 
   @override
-  getWidget() {
-    return new Image.network(photoUrl);
+  getWidget(BuildContext context) {
+
+    return new GestureDetector(
+      onTap: () => show(context),
+      child: new Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: new Row(
+          children: [
+            getDateWidget(),
+            new Icon(Icons.photo),
+          ]
+        )
+      )
+    );
+  }
+
+  show(context) {
+    showDialog<Null>(
+      context: context,
+      child: new AlertDialog(
+        content: new Image.network(photoUrl),
+        actions: [
+          new FlatButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text("Done")
+          ),
+        ]
+      )
+    );
   }
 }
 
@@ -56,17 +119,49 @@ class TextNote extends ClientNote {
     text = snapshot.value['text'];
   }
 
+  @override
   toFirebaseSet() {
-    return { "type": "text", "text": text };
+    var s = super.toFirebaseSet();
+    s.addAll({ "type": "text", "text": text });
+    return s;
   }
 
   @override
-  getWidget() {
-    return new Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: new Text(text) );
+  getWidget(BuildContext context) {
+
+    var sub = text;
+    if (text.length > 20 ) {
+      sub = text.substring(0,20);
+    }
+
+    return new GestureDetector(
+      onTap: () => show(context),
+      child: new Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: new Row(
+          children: [
+            getDateWidget(),
+            new Text(sub)
+          ]
+        )
+      )
+    );
   }
 
+  show(context) {
+    showDialog<Null>(
+      context: context,
+      child: new AlertDialog(
+        content: new Text(text),
+        actions: [
+          new FlatButton(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Text("Done")
+          ),
+        ]
+      )
+    );
+  }
 }
 
 class ClientNotesPage extends StatelessWidget {
@@ -123,9 +218,32 @@ class ClientNotesPage extends StatelessWidget {
     clientRef().update({ "photo_url": imageUrl.toString() });
   }
 
+  addImageNote(context) async {
+    File imageFile = await ImagePicker.pickImage();
+
+    int random = new Random().nextInt(100000);
+    StorageReference sref = FirebaseStorage.instance.ref().child("${user.googleId}_client_${client.key}_$random.jpg");
+    StorageUploadTask uploadTask = sref.put(imageFile);
+    Uri imageUrl = (await uploadTask.future).downloadUrl;
+
+    clientNotesRef().push().set(new PhotoNote(imageUrl.toString()).toFirebaseSet());
+  }
+
+  addTextNote(context) async {
+    String text = await new InputDialog(
+        title: "Enter Text Note",
+        labelText: "Type Here",
+        actionLabel: "Finished"
+    ).show(context);
+
+    if ( text != null ) {
+      clientNotesRef().push().set(new TextNote(text).toFirebaseSet());
+    }
+  }
+
   @override
   build(context) {
-    var controller = new TextEditingController();
+    // var controller = new TextEditingController();
 
     return
     new Stack(children: [
@@ -134,21 +252,20 @@ class ClientNotesPage extends StatelessWidget {
           leading: onTapGesture(client.getChip(), () => editClientPhoto(context) ),
           title: onTapGesture(new Text(client.name), () => editClientName(context)),
         ),
-        new TextField(
+        /*new TextField(
           controller: controller,
           decoration: new InputDecoration( labelText: "Enter a note"),
           onSubmitted: (t) {
             clientNotesRef().push().set(new TextNote(t).toFirebaseSet());
             controller.clear();
           }
-        ),
+        ), */
         new Expanded(child: new FirebaseAnimatedList(
-            reverse: true,
             sort: (a, b) => b.key.compareTo(a.key),
             query: clientNotesRef(),
             itemBuilder: (context, snapshot, animation, index) {
               ClientNote note = new ClientNote.fromFirebaseSnapshot(snapshot);
-              return note.getWidget();
+              return note.getWidget(context);
             }
           )
         )
@@ -156,24 +273,47 @@ class ClientNotesPage extends StatelessWidget {
       new Positioned(
           right: 10.0,
           bottom:10.0,
-          child:
-            new FloatingActionButton(
-              child: new Icon(Icons.camera),
-              tooltip: "Take a Photo",
-              onPressed: () async {
-
-                File imageFile = await ImagePicker.pickImage();
-
-                int random = new Random().nextInt(100000);
-                StorageReference sref = FirebaseStorage.instance.ref().child("${user.googleId}_client_${client.key}_$random.jpg");
-                StorageUploadTask uploadTask = sref.put(imageFile);
-                Uri imageUrl = (await uploadTask.future).downloadUrl;
-
-                clientNotesRef().push().set(new PhotoNote(imageUrl.toString()).toFirebaseSet());
-              }
-            )
+          child: new AddNoteContentMenu(addImageNote: addImageNote, addTextNote: addTextNote)
       )
     ]);
   }
 
+}
+
+typedef void ContextCallback(BuildContext context);
+
+class AddNoteContentMenu extends StatelessWidget {
+  final ContextCallback addImageNote;
+  final ContextCallback addTextNote;
+
+  AddNoteContentMenu({ this.addImageNote, this.addTextNote });
+
+  @override
+  build(context) {
+
+    final List<FabMiniMenuItem> fabList = [
+      new FabMiniMenuItem(
+        textColor: Colors.white,
+        chipColor: Colors.blue,
+        fabColor: Colors.blue,
+        icon: new Icon(Icons.camera),
+        elevation: 4.0,
+        text: "Photo",
+        tooltip: "Take a Photo",
+        onPressed: () => addImageNote(context)
+      ),
+      new FabMiniMenuItem(
+          textColor: Colors.white,
+          chipColor: Colors.blue,
+          fabColor: Colors.blue,
+          icon: new Icon(Icons.short_text),
+          elevation: 4.0,
+          text: "Text",
+          tooltip: "Add a Text Note",
+          onPressed: () => addTextNote(context)
+      ),
+    ];
+
+    return new FabDialer(fabList, Colors.blue, const Icon(Icons.add));
+  }
 }
