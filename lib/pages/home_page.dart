925dd5edf9d8d5c41ed176b94fc764bcd8c95d9b
Jason Stillwell <dragonfax@gmail.com>
2018-01-19 02:41:47 -0800

    fix app name in main home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -22,7 +22,7 @@ class HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Clients"),
+        title: new Text("Hairappy"),
         actions: <Widget>[ new TimerListButton() ]
       ),
       drawer: new ApplicationMenu(),
